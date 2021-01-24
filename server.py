6e07dead61b204c0ccba67a09462c50a24be6e4a
sekulchicky <stepan.kulchitsky@ya.ru>
2021-01-24 23:44:23 +0300

    Add emoji

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+import emoji
 from utils import get_number_of_styles
 from model import StyleModel
 
@@ -45,18 +46,20 @@ async def send_welcome(message: types.Message):
     user_style = 0
     n_styles = get_number_of_styles()
     await BotStates.waiting_select_style.set()
-    await message.reply("Привет!!!\nЯ neural-style-transfer бот.\n"
-                        "Я могу накладывать различные стили на фотографии.\n"
-                        "Для выбора стиля нажми /style, затем задай номер стиля и загрузи фотку.\n")
+    await message.reply(f"Привет!!!\n")
+    await message.answer("Я style-transfer бот \U0001F916\U0001F3A8\n")
+    await message.answer("C помощью нейросети, я могу стилизовать твою фотографию.\n"
+                         f"Чтобы посмотреть примеры стилизованных изображений нажми /example.\n"
+                         f"Чтобы выбрать стиль нажми /style, затем задай номер стиля и загрузи фотку.\n")
 
 
 @dp.message_handler(state=BotStates.waiting_select_style, commands=['style'])
 async def handle_style(message):
     global user_style
     if user_style == 0:
-        await message.reply(f"Для выбора стиля, набери число от 1 до {n_styles}.\n")
+        await message.reply(f"Для выбора стиля, набери число от 1 до {n_styles} \n")
     else:
-        await message.reply(f"Осталось загрузить какую-нибудь фотографию.\n")
+        await message.reply(f"Осталось загрузить какую-нибудь фотографию \n")
         await BotStates.waiting_photo.set()
 
 
@@ -66,16 +69,17 @@ async def style_select(message: types.Message):
     try:
         user_style = int(message.text.strip())
     except ValueError:
-        await message.reply("Некорректный ввод данных\n")
+        await message.reply("\U0000274EНекорректный ввод данных\n")
         return
     if 0 < user_style <= n_styles:
-        await message.answer(f"Поздравляю! Ты выбрал стиль под номером {user_style}.\n"
-                             f"Осталось загрузить какую-нибудь фотографию.\n")
+        await message.answer(f"\U00002705Ты выбрал стиль под номером {user_style}\n")
+
+        await message.answer("Осталось загрузить какую-нибудь фотографию \U0001F5BC\n")
         await BotStates.waiting_photo.set()
     else:
-        await message.reply("Некорректный ввод данных\n")
-        await message.answer("Стиля с таким номером не существует.\n"
-                             f"Попробуй ввести от 1 до {n_styles}.\n")
+        await message.reply("\U0000274EНекорректный ввод данных\n")
+        await message.answer("К сожалению стиля с таким номером не существует.\n"
+                             f"Попробуй ввести от 1 до {n_styles} \U0001F9D0\n")
 
 
 @dp.message_handler(state=BotStates.waiting_photo, content_types=['photo'])
@@ -94,12 +98,14 @@ async def handle_photo(message: types.Message):
 @dp.message_handler(state=BotStates.waiting_processing)
 async def handle_go_processing(message, content_image):
     global user_style
-    await message.answer("Я приступил к обработке фотографии. Это может занять какое-то время..\n"
-                         "...\n")
+    await message.answer("Я приступил к обработке фотографии.\n"
+                         f"Это может занять какое-то время \U000023F3\n")
+    await message.answer("\U0001F916...\n")
     output_image = await stylize(content_image)
     user_style = 0
     await bot.send_photo(chat_id=message.from_user.id, photo=output_image)
-    await message.answer("Посмотри как получилось! Если хочешь еще, жми /style.\n")
+    await message.answer("Готово! \U0001F64C\n")
+    await message.answer("Если хочешь еще, жми /style \U0001F60A\n")
     await BotStates.waiting_select_style.set()
 
 
@@ -109,6 +115,7 @@ async def stylize(content_image):
     model.load_model()
     output = model.run(content_image)
     return output
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
